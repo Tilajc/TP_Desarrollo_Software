c7a25b3380310d98b4ae2cf50c2399ebdad9baf2
tilajc <jctilatti@gmail.com>
2026-05-13 20:51:25 -0300

    Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Button } from './components/ui/button';
 function App() {
   return (
     <>
-      <section className="bg-black flex flex-col align-middle">
-        <h1 className="text-red-800">Hi</h1>
-        <div className="flex flex-wrap items-center gap-2 md:flex-row">
-          <Button variant="outline">Button</Button>
-        </div>
-      </section>
+      <div className="ml-36">
+        <section className="bg-black flex flex-col align-middle">
+          <h1 className="text-red-800">Hi</h1>
+          <div className="flex flex-wrap items-center gap-2 md:flex-row">
+            <Button variant="outline">Button</Button>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
