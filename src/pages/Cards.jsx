9b6299ed8972025ce8a6fd9eb46bba59e05e7908
tilajc <jctilatti@gmail.com>
2026-05-13 22:22:51 -0300

    Install and Configure React Router Dom + Link the sidebar navigation

---
+++ src/pages/Cards.jsx
@@ -0,0 +1,9 @@
+function Cards() {
+  return (
+    <section className="flex flex-col align-middle m-4">
+      <h1 className="">This is the cards screen</h1>
+    </section>
+  );
+}
+
+export default Cards;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Button } from '../components/ui/button';
+
+function Home() {
+  return (
+    <section className=" flex flex-col align-middle m-4">
+      <h1 className="">This is the home screen</h1>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button variant="outline">Button</Button>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
